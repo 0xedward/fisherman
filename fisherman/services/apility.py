0e550b15a656894a4288ce29f60bcd86f32e11cc
Edward <14011954+0xedward@users.noreply.github.com>
2019-10-10 23:36:16 -0700

    Refactor all logic into respective service file

--- fisherman/services/apility.py
+++ fisherman/services/apility.py
@@ -7,7 +7,7 @@ from fisherman.utils import colors
 # Documentation: https://apility.io/apidocs/#email-check
 BASE_URL = "https://api.apility.net/bademail/"
 
-def issuspicious(email, verbose_flag):
+def check_email_rep(email, verbose_flag):
     try:
         colors.print_gray('Casting line - sending email address to Apility')
         request_url = "{apility_url}{email_addr}".format(apility_url=BASE_URL, email_addr=email)
@@ -15,13 +15,15 @@ def issuspicious(email, verbose_flag):
         if response.status_code == 429:
             raise exceptions.RateLimitException()
         elif response.status_code == 404:
-            return None
+            colors.print_yellow("Apility does not currently have information about this email.")
         elif response.status_code == 200:
             api_data = json.loads(response.content)['response']
+            if api_data['email']['score'] < 0 or api_data['address']['score'] < 0 \
+                or api_data['score'] < 0:
+                colors.print_red("Apility has flagged the email as suspicious.")
+            else:
+                colors.print_green("Apility has not flagged the email as suspicious.")
             if verbose_flag:
                 colors.print_gray(json.dumps(api_data, indent=4))
-            return api_data['email']['score'] < 0 or api_data['address']['score'] < 0 \
-                or api_data['score'] < 0
     except exceptions.RateLimitException:
-        colors.print_pink("The ocean has dried up. Try to catch more phish later. \
-            Apility has rate limited you.")
+        colors.print_pink("The ocean has dried up. Try to catch more phish later. Apility has rate limited you.")
--- fisherman/services/emailrep.py
+++ fisherman/services/emailrep.py
@@ -7,7 +7,7 @@ from fisherman.utils import colors
 # Documentation: https://emailrep.io/docs/
 BASE_URL = "https://emailrep.io/"
 
-def issuspicious(email, verbose_flag):
+def check_email_rep(email, verbose_flag):
     try:
         colors.print_gray('Casting line - sending email address to EmailRep')
         request_url = "{email_rep_url}{email_addr}".format(email_rep_url=BASE_URL, email_addr=email)
@@ -16,9 +16,11 @@ def issuspicious(email, verbose_flag):
             raise exceptions.RateLimitException()
         elif response.status_code == 200:
             api_data = json.loads(response.content)
+            if api_data['suspicious']:
+                colors.print_red("EmailRep has flagged the email as suspicious.")
+            else:
+                colors.print_green("EmailRep has not flagged the email as suspicious.")
             if verbose_flag:
                 colors.print_gray(json.dumps(api_data, indent=4))
-            return api_data['suspicious']
     except exceptions.RateLimitException:
-        colors.print_pink("The ocean has dried up. Try to catch more phish later. \
-            EmailRep has rate limited you.")
+        colors.print_pink("The ocean has dried up. Try to catch more phish later. EmailRep has rate limited you.")
