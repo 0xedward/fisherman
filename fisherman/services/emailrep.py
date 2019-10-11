831f41cd0da1119de49ec910b3d6760eb72b41c0
Edward <14011954+0xedward@users.noreply.github.com>
2019-10-10 22:23:05 -0700

    Implement service module to call EmailRep and Apility

--- fisherman/services/emailrep.py
+++ fisherman/services/emailrep.py
@@ -1,17 +1,24 @@
+import json
 import requests
 
-from fisherman import utils
+from fisherman import exceptions
+from fisherman.utils import colors
 
-# https://emailrep.io/docs/
-
+# Documentation: https://emailrep.io/docs/
 BASE_URL = "https://emailrep.io/"
 
-def issuspicious(email):
-    utils.print_gray('Sending email to EmailRep')
-    request_URL = "{email_rep_url}{email_addr}".format(email_rep_url=BASE_URL, email_addr=email)
-    request_headers = {"Accept": "application/json"} 
-    response = requests.get(request_URL, headers=request_headers)
-    utils.validate_response(response)
-    return response.json()['suspicious']
-
-def isdomainsuspicious(email):
+def issuspicious(email, verbose_flag):
+    try:
+        colors.print_gray('Casting line - sending email address to EmailRep')
+        request_url = "{email_rep_url}{email_addr}".format(email_rep_url=BASE_URL, email_addr=email)
+        response = requests.get(request_url, headers={"Accept": "application/json"})
+        if response.status_code == 429:
+            raise exceptions.RateLimitException()
+        elif response.status_code == 200:
+            api_data = json.loads(response.content)
+            if verbose_flag:
+                colors.print_gray(json.dumps(api_data, indent=4))
+            return api_data['suspicious']
+    except exceptions.RateLimitException:
+        colors.print_pink("The ocean has dried up. Try to catch more phish later. \
+            EmailRep has rate limited you.")
--- fisherman/utils.py
+++
@@ -1,9 +0,0 @@
-def print_red(text): print("\033[91m {}\033[00m".format(text)) 
-def print_green(text): print("\033[92m {}\033[00m".format(text)) 
-def print_gray(text): print("\033[97m {}\033[00m".format(text))
-
-def validate_response(response):
-    if response.status_code == 200:
-        return True
-    elif response.status_code == 429:
-        
